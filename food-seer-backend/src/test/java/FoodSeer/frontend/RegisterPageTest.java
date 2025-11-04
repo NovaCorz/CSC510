8dc988183aaa4b83b2bdee0c6fdffa4a4237fe4c
afluffybunny7 <andrewalfredparr@yahoo.com>
2025-11-03 21:19:59 -0500

    Gave the template a default admin password

--- food-seer-backend/src/test/java/FoodSeer/frontend/RegisterPageTest.java
+++ food-seer-backend/src/test/java/FoodSeer/frontend/RegisterPageTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
